4dbe22fcefd30fed4fbef4c7612728569d077f9f
jpbamberg1993@gmail.com <jpbamberg1993@gmailc.om>
2015-09-30 23:13:55 -0400

    Initial app created and storing test data through browser.

---
+++ ember-cli-build.js
@@ -0,0 +1,13 @@
+/* global require, module */
+var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = function(defaults) {
+  var app = new EmberApp(defaults, {
+    // Add options here
+  });
+  app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+  app.import('bower_components/bootstrap/dist/css/bootstrap.css.map',{
+    destDir: 'assets'
+  })
+  return app.toTree();
+};
