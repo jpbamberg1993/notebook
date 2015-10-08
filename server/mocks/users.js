cb76b16f106625b3a3a6194d08870530c3112e23
jpbamberg1993@gmail.com <jpbamberg1993@gmail.com>
2015-10-08 19:31:22 -0400

    generated and configured http-mock for notes

--- server/mocks/users.js
+++ server/mocks/users.js
@@ -34,8 +34,10 @@ module.exports = function(app) {
 
   // Created by Ember CLI
   usersRouter.get('/', function(req, res) {
-    res.send({
-      'users': []
+    userDB.find(req.query).exec(function(error,users) {
+      res.send({
+        'users': []
+      });
     });
   });
 
